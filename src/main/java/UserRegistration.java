9e6e28afa4d83c9e0daa1b94b6ce57aec471cb51
Soun9922 <sagsounak39@gmail.com>
2022-08-16 19:03:35 +0530

    Added: Phone Number valid regex

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -4,6 +4,7 @@ public class UserRegistration {
 
     private static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
     private static final String EMAIL_PATTERN = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+    private static final String PHONE_NUMBER_PATTERN = "^[9]{1}[1]{1}[ ]?[6-9]{1}[0-9]{9}$";
 
     public boolean validateFirstName(String fName) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
@@ -18,5 +19,10 @@ public class UserRegistration {
     public boolean validateEmail(String email) {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
         return pattern.matches(EMAIL_PATTERN, email);
+    }
+
+    public boolean validatePhoneNumber(String phNumber) {
+        Pattern pattern = Pattern.compile(PHONE_NUMBER_PATTERN);
+        return pattern.matches(PHONE_NUMBER_PATTERN, phNumber);
     }
 }
